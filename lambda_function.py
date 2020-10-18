d5efaca2de1c25550e46768007eb0db3f0e7aecf
Nithin Vijayakumar <nv2ba@virginia.edu>
2020-10-17 23:46:17 -0400

    added filtering by instructors posts

--- lambda_function.py
+++ lambda_function.py
@@ -1,18 +1,63 @@
 import json
-import os
 from slack import WebClient
 from slack.errors import SlackApiError
 from piazza_api import Piazza
+import piazza_api
+import re
+from piazza_function import get_post_attr, pretty_print, pretty_print_instr
+import os
+
+def instructor_posts(my_class, instr_goal):
+    max_cid = my_class.get_statistics()['total']['questions'] - 1
+    cid = max_cid
+    instr_goal = min(instr_goal, 10)
+    num_instr = 0
+    text = ""
+    while(num_instr < instr_goal and cid > (max_cid - 25)):
+        try:
+            post = my_class.get_post(cid)
+            all_post_attr = get_post_attr([post])
+            post_text = pretty_print_instr(all_post_attr[0])
+            if post_text != "":
+                text+=post_text
+                num_instr+=1
+        except piazza_api.exceptions.RequestError:
+            print(cid)
+            pass
+        cid-=1
+    return text
 
 def lambda_handler(event, context):
     p = Piazza()
     p.user_login(os.environ['piazza_email'], os.environ['piazza_password'])
-
+    user_profile = p.get_user_profile()
+    myEvent = event['event']
     client = WebClient(token=os.environ['slack_token'])
-    try:
-        response = client.chat_postMessage(
-        channel='#random',
-        text="Hello world!")
-        assert response["message"]["text"] == "Hello world!"
-    except SlackApiError as e:
-        print(e)
+
+
+    if(myEvent['type'] == 'app_mention'):
+        if(('Show me the last' in myEvent['text']) and ('piazza post(s)' in myEvent['text'])):
+            question = myEvent['text']
+            num_recent = (question.replace('Show me the last ', '').replace(' piazza post(s)', '')).strip().split(' ', 2)[1]
+            my_class = p.network(os.environ["CS2110"])
+            posts = my_class.iter_all_posts(limit=int(num_recent))
+            all_post_attr = get_post_attr(posts)
+            my_text = ""
+            for post in all_post_attr:
+                my_text += pretty_print(post)
+
+            response = client.chat_postMessage(
+                channel=myEvent['channel'],
+                text=my_text)
+        elif(('Show me the last' in myEvent['text']) and ('instructor post(s)' in myEvent['text'])):
+            print(f"Event: {event}")
+            print(f"**************")
+            num_recent = re.search(r"(\s(\d+)\s)", myEvent['text'])[0].strip()
+            print(myEvent['text'])
+            print("Where is this? Recent:", num_recent, ":after")
+            my_class = p.network(os.environ['CS2110'])
+            my_text = instructor_posts(my_class, int(num_recent))
+            response = client.chat_postMessage(
+                channel=myEvent['channel'],
+                text=my_text)
+            
--- piazza_function.py
+++ piazza_function.py
@@ -1,10 +1,11 @@
 import os
 from piazza_api import Piazza
+import piazza_api
 import html
 from bs4 import BeautifulSoup
 
-os.environ['PIAZZA_USERNAME'] = "nv2ba@virginia.edu"
-os.environ['PIAZZA_PASSWORD'] = "***********"
+#os.environ['PIAZZA_USERNAME'] = "nv2ba@virginia.edu"
+#os.environ['PIAZZA_PASSWORD'] = "******"
 
 def get_child_post(children):
     ret = []
@@ -12,43 +13,66 @@ def get_child_post(children):
         return ret
     else:
         for child in children:
+            child_dict = {}
             if "subject" not in child.keys():
-                ret.append({
-                    'text': child['subject'],
-                    'children': get_child_post(child['children'])
-                })
+                child_dict['text'] = child['history'][0]['content']
             else:
-                ret.append({
-                    'text': child['subject'],
-                    'children': get_child_post(child['children'])
-                })
+                child_dict['text'] = child['subject']
+            child_dict['type'] = "Instructor" if child['type'] == "i_answer" else "Student"
+            child_dict['children'] = get_child_post(child['children'])
+            ret.append(child_dict)
         return ret
-
-def get_post_attr(posts):
-    all_post_attr = []
-    for post in posts:
-        all_post_attr.append({ 
-            'content' : post['history'][-1]['content'], #most recent post
-            'children': get_child_post(post['children'])
-        })
-    return all_post_attr
 
 def clean_text(text):
     soup = BeautifulSoup(text, 'html.parser')
     return html.unescape(soup.get_text())
 
-def pretty_print(post_dict):
-    print("\n ****************************************** NEW POST ******************************************\n")
+def get_post_attr(posts):
+    all_post_attr = []
+    for post in posts:
+        all_post_attr.append({ 
+            'title': post['history'][0]['subject'],
+            'content' : post['history'][0]['content'], #most recent post
+            'children': get_child_post(post['children'])
+        })
+        # for i, content in enumerate(post['history']):
+        #     print(f"History edit: {i}")
+        #     print(clean_text(content['content']))
 
-    print(f"Main: {clean_text(post_dict['content'])}")
-    print("\n**************************\n")
+    
+    return all_post_attr
+
+
+def pretty_print(post_dict):
+    text = ""
+    text += ("\n ****************************************** POST ******************************************\n")
+    text += (f"```{clean_text(post_dict['content'])}```")
     for comment in post_dict['children']:
-        print(f"\t1st level Comment: {clean_text(comment['text'])}")
-        print("\n**************************\n")
+        text += (f"\n>```{(clean_text(comment['text']))}```")
+        text += ("\n")
         if len(comment['children']) != 0:
             for child_comment in comment['children']:
-                print(f"\t\t2nd level Comment: {clean_text(child_comment['text'])}")
-                print("\n**************************\n")
+                text += (f"\n>```{(clean_text(child_comment['text']))}```")
+                text += ("\n")
+    text += ("\n")
+    return text
+
+def pretty_print_instr(post_dict):
+    text = ""
+    text += ("\n ****************************************** POST ******************************************\n")
+    if(any([child['type'] == "Instructor" for child in post_dict['children']])): #are there instructor answers?
+        text += (f"{clean_text(post_dict['title'])}\n")
+        text += (f"```{clean_text(post_dict['content'])}```")
+        for comment in post_dict['children']:
+            if comment['type'] == "Instructor":
+                text += (f"\n>```{comment['type']}: {(clean_text(comment['text']))}```")
+                text += ("\n")
+                if len(comment['children']) != 0:
+                    for child_comment in comment['children']:
+                        text += (f"\n>```{child_comment['type']}: {(clean_text(child_comment['text']))}```")
+                        text += ("\n")
+
+    return text if "Instructor" in text else ""
 
 
 
@@ -57,11 +81,43 @@ if __name__ == "__main__":
 
     p.user_login(email=os.environ['PIAZZA_USERNAME'], password=os.environ['PIAZZA_PASSWORD'])
 
-    num_recent = 4
-    cs2150 = p.network("k5bqcfbzltk49c")
-    posts = cs2150.iter_all_posts(limit=num_recent)
+    cs2110 = p.network("jzqhh4bax85av")
 
-    all_post_attr = get_post_attr(posts)
+    max_cid = cs2110.get_statistics()['total']['questions'] - 1
+    cid = max_cid
+    instr_goal = 10
+    num_instr = 0
+    text = ""
+    iters = 0
+    while(num_instr < instr_goal and cid > (max_cid - 25)):
+        iters+=1
+        try:
+            post = cs2110.get_post(cid)
+            all_post_attr = get_post_attr([post])
+            post_text = pretty_print_instr(all_post_attr[0])
+            if post_text != "":
+                text+=post_text
+                num_instr+=1
+        except piazza_api.exceptions.RequestError:
+            print(cid)
+            pass
+        cid-=1
+    
+    print(text)
+    print(f"max: {max_cid}\t cid:{cid}")
+    print(iters)
 
-    for post in all_post_attr:
-        pretty_print(post)
+    # num_recent = 7
+    # cs2150 = p.network("jzqhh4bax85av")
+    
+    # # posts = cs2150.iter_all_posts(limit=num_recent)
+    # # all_post_attr = get_post_attr(posts)
+    
+    # all_post_attr = get_post_attr([cs2150.get_post(cid) for cid in range(900, 906)])
+
+    # #post = cs2150.get_post(1090)
+    # #all_post_attr = get_post_attr([post])
+
+    # for post in all_post_attr:
+    #     pass
+    #     #print(pretty_print_i(post))
